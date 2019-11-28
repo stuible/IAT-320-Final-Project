5a1582958bfa5617c9e1b5615b34b137a9f44c20
Josh Stuible <shrewdlove@gmail.com>
2019-11-28 01:52:11 -0800

    Renamed projext file to be correct, demo pot code added

--- Object1/Object1.ino
+++ Object1/Object1.ino
@@ -5,47 +5,37 @@
 
 Transmit transmit;
 
-int objectChannel = 3;
+int objectChannel = 2;
 int baseChannel = 1;
 
 const int volumePin = A0;
-const int AMFMPin =  A1;
 
 int prevVolume = 0;
-int prevAM;
 
-//350 - 1023
+//0 - 1020
 
 void setup() {
   Serial.begin(9600);
 
-  transmit = Transmit(objectChannel, baseChannel);
+//  transmit = Transmit(objectChannel, baseChannel);
 
-  prevAM = analogRead(AMFMPin) > 500 ? true : false;
 
 }
 void loop() {
-  transmit.receive();
+//  transmit.receive();
 
   //  transmit.send("track", "next");
 
   int volume = analogRead(volumePin);
-  int AM = analogRead(AMFMPin) > 500 ? true : false;
 
   if (abs(abs(prevVolume) - abs(volume)) > 25) {
     //    Serial.println("cahnged enough");
-    transmit.send("vol", (int) map(volume, 350, 1023, 0, 100));
+//    transmit.send("vol", (int) map(volume, 350, 1023, 0, 100));
     prevVolume = volume;
   }
 
-  if (prevAM != AM) {
-    transmit.send("AM", AM);
-    prevAM = AM;
-  }
-
-
-  //  Serial.print("volume: ");
-  //  Serial.println(volume);
+    Serial.print("volume: ");
+    Serial.println(volume);
 
   //    Serial.print("AM / FM: ");
   //    Serial.println(AMFM);
